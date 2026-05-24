56c18a60a9b142501ba64a5be7c0c9114de57879
Aldo Ortega <aldortega65@gmail.com>
2026-05-24 18:03:16 -0400

    Github pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+const base = process.env.GITHUB_ACTIONS && repositoryName ? `/${repositoryName}/` : '/'
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [vue()],
 })
